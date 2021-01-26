5b10059b986a24d849903218d86930a90453d14e
Da Fonseca Damien <43819224+DaFonsecaDamien@users.noreply.github.com>
2021-01-26 23:32:14 +0100

    [ADD] Initial Project

---
+++ src/main/java/esgi/fr/App.java
@@ -0,0 +1,13 @@
+package esgi.fr;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
